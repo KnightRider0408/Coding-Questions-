2e258a9ebedbebe59b032e29243d6c58aca9d9d1
Vikas Prajapati <vikas.master0408@gmail.com>
2024-12-29 21:02:19 +0530

    Create LeetCode-Q.13.py
    Solution in Python.

---
+++ LeetCode-Q.13.py
@@ -0,0 +1,16 @@
+#Hello, my approach towards the roman no. to integer Conversion is given below.
+#My Code in Python is :-
+
+class Solution(object):
+    def romanToInt(self, s):
+        roman = { "I":1, "V":5 , "X":10, "L":50, "C": 100 , "D": 500, "M": 1000 }
+        ans=0
+        for a,b in zip(s,s[1:]):
+            if roman[a]<roman[b]:
+                ans-=roman[a]
+            else:
+                ans+=roman[a]
+        return ans + roman[s[-1]]
+
+
+# It runs effectively.
